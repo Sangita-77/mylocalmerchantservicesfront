67a580032c830c91fe85ea050fc47ff15d688f4c
Sangita <sangita@dreamlogodesign.com>
2025-07-02 20:02:34 +0530

    redirect merchnat list

--- src/components/ButtonLarge.jsx
+++ src/components/ButtonLarge.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './../styles/styles.css';
+import { useNavigate } from "react-router-dom";
+import { routes } from "./../utils/routes";
 
-const ButtonLarge = ({title, icon, customStyle, onClick}) => {
+const ButtonLarge = ({title, icon}) => {
+
+  const navigate = useNavigate();
   return (
-    <div className="button_large" onClick={onClick}>
+    <div className="button_large" onClick={() => navigate(routes.merchant_list())}>
         <p className='large_button_text'>{title}</p>
         {icon}
     </div>
