6892079ddadfd2617887c2f1cef0d04cb23bbd54
Sangita <sangita@dreamlogodesign.com>
2025-07-10 13:19:26 +0530

    add loader on contact list

--- src/pages/admin/ContactList.jsx
+++ src/pages/admin/ContactList.jsx
@@ -6,6 +6,7 @@ import { BASE_URL } from "../../utils/apiManager";
 import { AppContext } from "../../utils/context";
 import { AiOutlineDelete } from "react-icons/ai";
 import { PiEyeLight } from "react-icons/pi";
+import PreLoader from "../../components/PreLoader";
 
 const ContactList = () => {
 
@@ -54,50 +55,65 @@ const ContactList = () => {
 
         <div className="contactlistContainer">
           <div className="accordion" id="accordionExample">
-            {ContactList.contactList.map((contact, index) => (
-              <div className="accordion-item" key={contact.contact_id}>
-                <h2 className="accordion-header" id={`heading${index}`}>
-                  <button
-                    className={`accordion-button ${index !== 0 ? 'collapsed' : ''}`}
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target={`#collapse${index}`}
-                    aria-expanded={index === 0 ? "true" : "false"}
-                    aria-controls={`collapse${index}`}
+            {loading ? (
+              <tr>
+                <td colSpan="5" style={{ textAlign: "center", padding: "20px" }}>
+                  <PreLoader />
+                </td>
+              </tr>
+            ) : Array.isArray(ContactList.contactList) && ContactList.contactList.length > 0 ? (
+              ContactList.contactList.map((contact, index) => (
+                <div className="accordion-item" key={contact.contact_id}>
+                  <h2 className="accordion-header" id={`heading${index}`}>
+                    <button
+                      className={`accordion-button ${index !== 0 ? 'collapsed' : ''}`}
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#collapse${index}`}
+                      aria-expanded={index === 0 ? "true" : "false"}
+                      aria-controls={`collapse${index}`}
+                    >
+                      {contact.name}
+                    </button>
+                  </h2>
+                  <div
+                    id={`collapse${index}`}
+                    className={`accordion-collapse collapse ${index === 0 ? 'show' : ''}`}
+                    aria-labelledby={`heading${index}`}
+                    data-bs-parent="#accordionExample"
                   >
-                    {contact.name}
-                  </button>
-                </h2>
-                <div
-                  id={`collapse${index}`}
-                  className={`accordion-collapse collapse ${index === 0 ? 'show' : ''}`}
-                  aria-labelledby={`heading${index}`}
-                  data-bs-parent="#accordionExample"
-                >
-                  <div className="accordion-body">
-                    <div>Email : {contact.email}</div><br></br>
-                    <div>Phone : {contact.phone}</div><br></br>
-                    <div>Message : {contact.message}</div>
-                    {/* <table className="table">
-                      <tbody>
-                        <tr>
-                          <td>Email :</td>
-                          <td>{contact.email}</td>
-                        </tr>
-                        <tr>
-                          <td>Phone:</td>
-                          <td>{contact.phone}</td>
-                        </tr>
-                        <tr>
-                          <td>Message:</td>
-                          <td>{contact.message}</td>
-                        </tr>
-                      </tbody>
-                    </table> */}
+                    <div className="accordion-body">
+                      <div>Email : {contact.email}</div><br></br>
+                      <div>Phone : {contact.phone}</div><br></br>
+                      <div>Message : {contact.message}</div>
+                      {/* <table className="table">
+                        <tbody>
+                          <tr>
+                            <td>Email :</td>
+                            <td>{contact.email}</td>
+                          </tr>
+                          <tr>
+                            <td>Phone:</td>
+                            <td>{contact.phone}</td>
+                          </tr>
+                          <tr>
+                            <td>Message:</td>
+                            <td>{contact.message}</td>
+                          </tr>
+                        </tbody>
+                      </table> */}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+              )
+            ) : (
+              <tr>
+                <td className="td" colSpan="5" style={{ textAlign: "center" }}>
+                  No User Found.
+                </td>
+              </tr>
+            )}
           </div>
         </div>
 
