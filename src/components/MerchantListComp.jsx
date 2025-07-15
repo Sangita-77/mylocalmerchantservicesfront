61f8bee41f083067b72b27bf0791c42729adce6d
Sangita <sangita@dreamlogodesign.com>
2025-07-15 18:49:36 +0530

    implement the merchant list page functionality on super admin

--- src/components/MerchantListComp.jsx
+++ src/components/MerchantListComp.jsx
@@ -1,39 +1,71 @@
 import React from "react";
 import AccordianProps from "./AccordianProps";
 import { MdOutlineGroup } from "react-icons/md";
+import { PiEyeLight } from "react-icons/pi";
+import { GoPencil } from "react-icons/go";
+import { AiOutlineDelete } from "react-icons/ai";
 
 
 
-const MerchantListComp = ({ text }) => {
-
-   const users = [
-      { id: 1, name: 'Alice', age: 30, city: 'New York' },
-      { id: 2, name: 'Bob', age: 24, city: 'London' },
-      { id: 3, name: 'Charlie', age: 35, city: 'Paris' },
-    ];
+const MerchantListComp = ({ approvedUsers = [], pendingUsers = [] }) => {
   return (
-
-  <div>
-    <AccordianProps 
-    bgColor= "#71CDEA"
-    Icon={MdOutlineGroup}
-    Heading="Approved Merchant"
-    tbody={
-      <>
-      {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.age}</td>
-                  <td>{user.city}</td>
-                </tr>
-        ))}
-      </>
-    }
-    />
-  </div>
-
+    <div>
+      <AccordianProps 
+        bgColor="#71CDEA"
+        Icon={MdOutlineGroup}
+        Heading="Approved Merchant"
+        tbody={
+          <>
+            {approvedUsers.map((user, index) => (
+              <tr key={user.id || index}>
+                <td>{user.merchant_id}</td>
+                <td>{user.merchant_name}</td>
+                <td>{user.street} , {user.city} , Zip - {user.zip_code}</td>
+                <td>{user.type_of_service}</td>
+                <td>
+                  <button className="viewButton" data-bs-toggle="tooltip" data-bs-placement="auto" title="View Details">
+                    <PiEyeLight size={22} color="white" />
+                  </button>
+                  <button className="editButton" data-bs-toggle="tooltip" data-bs-placement="auto" title="Delete">                               
+                    <GoPencil />
+                  </button>
+                 
+                </td>
+              </tr>
+            ))}
+          </>
+        }
+      />
+      
+      <AccordianProps 
+        bgColor="#71CDEA"
+        Icon={MdOutlineGroup}
+        Heading="Pending Merchant"
+        tbody={
+          <>
+            {pendingUsers.map((user, index) => (
+              <tr key={user.id || index}>
+                <td>{user.merchant_id}</td>
+                <td>{user.merchant_name}</td>
+                <td>{user.street} , {user.city} , Zip - {user.zip_code}</td>
+                <td>{user.type_of_service}</td>
+                <td>
+                  <button className="viewButton" data-bs-toggle="tooltip" data-bs-placement="auto" title="View Details">
+                    <PiEyeLight size={22} color="white" />
+                  </button>
+                  <button className="delButton" data-bs-toggle="tooltip" data-bs-placement="auto" title="Delete">                               
+                    <AiOutlineDelete size={22} color="#E60E4E" style={{ cursor: "pointer" }} />
+                  </button>
+                  
+                </td>
+              </tr>
+            ))}
+          </>
+        }
+      />
+    </div>
   );
 };
+
 
 export default MerchantListComp;
--- src/pages/admin/AdminMerchantList.jsx
+++ src/pages/admin/AdminMerchantList.jsx
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from "react";
 import './../../styles/styles.css';
 import AdminDashBoardTopBar from "../../components/AdminDashBoardTopBar";
 import MerchantListComp from "../../components/MerchantListComp";
+import axios from "axios";
+import { BASE_URL } from "../../utils/apiManager";
+import { AppContext } from "../../utils/context";
+
 
 const AdminMerchantList = () => {
+
+  const { token } = useContext(AppContext);
+  const [approvedUsers, setApprovedUsers] = useState([]);
+  const [pendingUsers, setPendingUsers] = useState([]);
+
+  useEffect(() => {
+    fetchapprovedUsers();
+    fetchpendingUsers();
+  }, [token]);
+
+  const fetchapprovedUsers = async () => {
+    try {
+        const response = await axios.post(
+            `${BASE_URL}/getMerchantsList`,
+            { status: 1 , flag: "merchant services providers" },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        if (response.data.status) {
+          console.log("Approveddddddddddddd",response.data);
+          setApprovedUsers(response.data.getMerchantList);
+        } else {
+            console.error(response.data.message);
+        }
+    } catch (error) {
+        console.error("API Error:", error);
+    }
+
+  };
+
+  const fetchpendingUsers = async () => {
+    try {
+        const response = await axios.post(
+            `${BASE_URL}/getMerchantsList`,
+            { status: 0 , flag: "merchant services providers" },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        if (response.data.status) {
+          console.log("pendinggggggggggg",response.data);
+          setPendingUsers(response.data.getMerchantList);
+        } else {
+            console.error(response.data.message);
+        }
+    } catch (error) {
+        console.error("API Error:", error);
+    }
+
+  };
   return (
      <div className="adminUserlistWrapper merchantListAdmin">
       <div className="adminDashboardContainer">
             <AdminDashBoardTopBar heading="Merchant List" />
               <div className='adminUserlIstContainer'>
-                 <MerchantListComp/>
+                 <MerchantListComp approvedUsers={approvedUsers} pendingUsers={pendingUsers}/>
                </div>
         </div>
     </div>
