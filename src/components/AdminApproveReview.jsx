185f039e21ffd13c60c47624399006140e780ccf
Souvick <souvik@dreamlogodesign.net>
2025-11-10 21:11:32 +0530

    add some style in admin review list

--- src/components/AdminApproveReview.jsx
+++ src/components/AdminApproveReview.jsx
@@ -7,7 +7,7 @@ const AdminApproveReview = () => {
 
   return (
     <>
-      <div className="accordionWrap">
+      <div className="accordionWrap approveReviewList">
         <div className="merchantaccordian-table">
           <Accordion defaultActiveKey={["0"]} alwaysOpen>
             <Accordion.Item eventKey="0">
